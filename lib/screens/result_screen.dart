1da1d5082006e48c2b4f8d04bd8076c8d42eb1ef
Reham salama <rehamsalamaali@hotmail.com>
2024-08-12 23:59:56 +0300

    add app routes

---
+++ lib/screens/result_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResultScreen extends StatelessWidget {
+  const ResultScreen({super.key});
+
+  static const String id = "result screen";
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
